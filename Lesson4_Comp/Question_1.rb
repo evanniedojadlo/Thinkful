1124a8199f7b7317fb898c34237c252acf981ae3
evanniedojadlo <evanniedojadlo@gmail.com>
2013-12-12 14:07:17 -0600

    Adding README File

--- Lesson4_Comp/Question_1.rb
+++ Lesson4_Comp/Question_1.rb
@@ -1,4 +1,5 @@
 #What is the purpose of a loop?
+
 #To repeat an action, an example of this is prompting a user a certain type of input and #if they answer incorrectly the program will re-prompt them until the input becomes true.
 
 i = 0
---
+++ README.md
@@ -0,0 +1,1 @@
+Projects for Thinkful.com RoR track
