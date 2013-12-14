048f0904419b815261e3fcec7274a4874c486183
evanniedojadlo <evanniedojadlo@gmail.com>
2013-12-14 17:09:11 -0600

    Adding Version One of the Tip Calculator

---
+++ Tip_Calculator/version_one/string_format_test.rb
@@ -0,0 +1,5 @@
+#Trying out string interpolation
+
+tax = 0.12
+
+puts "The amount of tax is #{tax}"
--- Tip_Calculator/version_one/version_1.rb
+++ Tip_Calculator/version_one/version_1.rb
@@ -1,3 +1,15 @@
+#Tip Calculator with Hard-Coded Variables
+
 meal = 20.00
-tax = .12
-tip = .20
+tax = 12.to_f
+tip = 20.to_f
+
+tax_value = meal * tax / 100
+meal_with_tax = meal + tax_value
+tip_value = meal * tip / 100
+total_cost = meal_with_tax + tip_value
+
+puts "The pre-tax cost of your meal was #{meal}."
+puts "At #{tax}%, tax for this meal is #{meal_with_tax}."
+puts "For a #{tip}% tip, you should leave #{tip_value}."
+puts "The grand total for this meal is then #{total_cost}."
